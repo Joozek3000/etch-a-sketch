d63233429a24cb95fd71e4ad72d65142971fa999
daedalus <j.jozefczyk90@gmail.com>
2022-08-28 10:02:29 +0100

    added grid borders and erase buttons functionality, changed js script

--- script.js
+++ script.js
@@ -1,30 +1,43 @@
 const grid = document.getElementById('grid');
-const sizer = document.getElementById('sizer');
+const size = document.getElementById('size');
 const eraser = document.getElementById('eraser');
 const color = document.getElementById('color');
+const gridBorder = document.getElementById('grid-borders');
 
 // grid
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement('div');
-  row.className = 'row';
-  for (let j = 0; j < 16; j++) {
-    let box = document.createElement('div');
-    box.className = 'box';
-    row.appendChild(box);
+function makeGrid(number) {
+  grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+  for (let i = 0; i < number * number; i++) {
+    let cell = document.createElement('div');
+    grid.appendChild(cell).id = 'box';
+    cell.classList.add('border');
+    cell.classList.add('box');
+    cell.style.backgroundColor = 'white';
   }
-  document.getElementById('grid').appendChild(row);
+  size.textContent = `${number} x ${number}`;
 }
+makeGrid(16);
 
-// change color on mouseover
-color.addEventListener('mouseover', function () {
+// drawing on hover
+color.addEventListener('click', function () {
   grid.addEventListener('mouseover', function (e) {
-    e.target.style.backgroundColor = 'black';
+    e.target !== grid ? (e.target.style.backgroundColor = 'black') : null;
   });
 });
 
 // erase functionality
 eraser.addEventListener('click', function () {
   grid.addEventListener('mouseover', function (e) {
-    e.target.style.backgroundColor = 'white';
+    e.target !== grid ? (e.target.style.backgroundColor = 'white') : null;
+  });
+});
+
+// grid borders
+const allBoxes = document.querySelectorAll('.box');
+gridBorder.addEventListener('click', function () {
+  allBoxes.forEach((box) => {
+    box.classList.toggle('no-border');
+    box.classList.toggle('border');
   });
 });
