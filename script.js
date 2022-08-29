651f1a2a5628bfedd719650fd9e913914cf4dbc4
daedalus <j.jozefczyk90@gmail.com>
2022-08-29 13:57:47 +0100

    added grid borders functionallity, various changes in code

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const size = document.getElementById('size');
 const eraser = document.getElementById('eraser');
 const color = document.getElementById('color');
 const gridBorder = document.getElementById('grid-borders');
+const clear = document.getElementById('clear');
 
 // grid
 function makeGrid(number) {
+  number = number || 16;
+  let cellWidth = 40 / number + 'rem';
+  let cellHeight = 40 / number + 'rem';
   grid.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${number}, 1fr)`;
   for (let i = 0; i < number * number; i++) {
@@ -14,15 +18,22 @@ function makeGrid(number) {
     cell.classList.add('border');
     cell.classList.add('box');
     cell.style.backgroundColor = 'white';
+    cell.style.width = cellWidth;
+    cell.style.height = cellHeight;
+    cell.addEventListener('mouseover', function (e) {
+      e.target !== grid ? (e.target.style.backgroundColor = 'black') : null;
+    });
   }
   size.textContent = `${number} x ${number}`;
 }
-makeGrid(16);
+makeGrid();
 
 // drawing on hover
-color.addEventListener('click', function () {
+color.addEventListener('input', function () {
+  let newColor = document.getElementById('input-color').value;
+  console.log(newColor);
   grid.addEventListener('mouseover', function (e) {
-    e.target !== grid ? (e.target.style.backgroundColor = 'black') : null;
+    e.target !== grid ? (e.target.style.backgroundColor = newColor) : null;
   });
 });
 
@@ -41,3 +52,20 @@ gridBorder.addEventListener('click', function () {
     box.classList.toggle('border');
   });
 });
+
+// clear button
+clear.addEventListener('click', function () {
+  allBoxes.forEach((box) => {
+    box.style.backgroundColor = 'white';
+  });
+});
+
+// size button
+// size.addEventListener('click', function () {
+//   let number = prompt(`Enter grid size less or equal to 100`);
+//   if (number !== Number.isInteger()) {
+//     return;
+//   } else if (number > 100) {
+//     number = prompt(`Enter grid size greater or equal to 100`);
+//   }
+// });
